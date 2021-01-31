fe5bf896e1dba9591617113391ed2ac4f6aad176
SSGrady <gradystevena@gmail.com>
2021-01-30 21:10:02 -0500

    Form creation attempt.. ended up corrupting pip

--- UI/server.py
+++ UI/server.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+from forms import SignUpForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'freeCofee'
+							#Some random key/something that allows us to configure
+							# our Flask application
+							#This is ESSENTIAL if you want form to be up and running
 
 @app.route('/')
 def home():
@@ -22,8 +27,14 @@ def blog():
 									 # a data type i.e. i could've written <int:blog_id>
 									 #or simply <blog_id>
 							#what carrots <> do is specify a rule
+
 def blogpost(blog_id):
 	return 'This is blog post number ' + blog_id #printing out the blog id #
+
+@app.route('/signup')
+def signup():
+	form = SignUpForm()
+	return render_template('signup.html', form=form)
 
 
 if __name__ == '__main__':
